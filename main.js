5561f956469c2152598f4a30b03f110c5a590528
Mahliyo <gasandigital13@gmail.com>
2026-03-23 22:29:05 +0500

    juft sonlarni qaytaring yakunlandi

--- main.js
+++ main.js
@@ -26,6 +26,9 @@
 //  console.log(kichik);
 
 // 1-masala
-let nums = [3, 6, 8, 11, 14, 19 ];
+// let nums = [3, 6, 8, 11, 14, 19 ];
+// let num = nums.filter(num => num % 2  !== 0);
+// console.log(num);
+
 
  
